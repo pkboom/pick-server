7ae1dd5f08cfd8c5bd74f0fe25013531a35d597c
Keunbae Park <pkboom@users.noreply.github.com>
2022-09-25 13:27:40 -0400

    wip

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,10 @@
 
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
-
 use Inertia\Inertia;
-
-// frontend: hit backend every 1 sec
-// backend: if file updated, dump new content
-// if not dump same content or do nothing. which one? we will see
 
 Route::get('/', function () {
     // get update time of file
@@ -17,21 +14,20 @@ Route::get('/', function () {
     if ($updateTime !== Cache::get('pick_time')) {
         Cache::put('pick_time', filemtime(Config::get('pick.file')));
 
-        // or else dump new stuff
         $data = file_get_contents(Config::get('pick.file'));
 
-        dump($data);
         dump($data);
 
         return;
     }
 
-    // if ther are the same don't do anyting
     return Inertia::render('Pick', [
         'noDump' => true,
     ]);
 });
 
 Route::post('webhook', function () {
-    return view('welcome');
+    file_put_contents(Config::get('pick.file'), Request::all());
+
+    return Response::noContent();
 });
